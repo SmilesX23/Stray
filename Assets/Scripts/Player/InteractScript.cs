36cd034f9348157f3eeade37a313a8ae6e2e59a6
OliverJayMorrison <oliverjaymorrison@gmail.com>
2016-10-07 20:18:21 +0100

    Iteration 5
    Raycasting implemented to prompt users to interact with AI

--- Assets/Scripts/Player/InteractScript.cs
+++ Assets/Scripts/Player/InteractScript.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class InteractScript : MonoBehaviour {
 
+
+	public Text interactText;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.forward,out hit, 3)) 
+		{
+			if(hit.collider.gameObject.tag == "AI")
+			{
+				interactText.enabled = true;
+			}
+		} 
+		else 
+		{
+			interactText.enabled = false;
+		}
 	}
 }
