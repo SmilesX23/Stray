635e31c75d4f538b716bf4f8e4ae57fcebe9fa17
OliverJayMorrison <oliverjaymorrison@gmail.com>
2016-10-12 20:37:13 +0100

    Iteration 10.0.2
    Dead reckoning updated.

--- Assets/Scripts/Player/DeadReckoning.cs
+++ Assets/Scripts/Player/DeadReckoning.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class DeadReckoning : MonoBehaviour {
 
+	float lastSyncTime = 0f;
+	float syncDelay = 0f;
+	float syncTime = 0f;
+	Vector3 startPosition = Vector3.zero;
+	Vector3 endPosition = Vector3.zero;
+
+
+
+
 	// Use this for initialization
 	void Start ()
     {
@@ -12,15 +21,18 @@ public class DeadReckoning : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	
+		syncTime += Time.deltaTime;
+		if
+			(syncTime < syncDelay)
+		{
+			transform.position = Vector3.Lerp(startPosition, endPosition, syncTime / syncDelay);
+		}
+
+
 	}
 
 
-    float lastSyncTime = 0f;
-    float syncDelay = 0f;
-    float syncTime = 0f;
-    Vector3 startPosition = Vector3.zero;
-    Vector3 endPosition = Vector3.zero;
+    
 
 
 
