812058eaa4fb59ee69ff11d9e05b08841c6de87c
Oliver Morrison <oliverjaymorrison@gmail.com>
2016-10-21 14:55:00 +0100

    Iteration 10.0.4
    Prepped the Player prefab to have the dead reckoning script attached and
    setup by default

--- Assets/Scripts/Player/DeadReckoning.cs
+++ Assets/Scripts/Player/DeadReckoning.cs
@@ -18,8 +18,30 @@ public class DeadReckoning : Photon.MonoBehaviour, IPunObservable
 		this.onUpdatePos = transform.position;
 	}
 
+    public void Update()
+    {
+        if (this.photonView.isMine)
+        {
+            return;     // if this object is under our control, we don't need to apply received position-updates 
+        }
 
-	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        // We get 10 updates per sec. Sometimes a few less or one or two more, depending on variation of lag.
+        // Due to that we want to reach the correct position in a little over 100ms. We get a new update then.
+        // This way, we can usually avoid a stop of our interpolated cube movement.
+        //
+        // Lerp() gets a fraction value between 0 and 1. This is how far we went from A to B.
+        //
+        // So in 100 ms, we want to move from our previous position to the latest known. 
+        // Our fraction variable should reach 1 in 100ms, so we should multiply deltaTime by 10.
+
+
+        this.fraction = fraction + Time.deltaTime * 10;
+        transform.localPosition = Vector3.Lerp(this.onUpdatePos, this.latestCorrectPos, this.fraction); // set our pos between A and B
+    }
+
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 	{
 		if (stream.isWriting)
 		{
@@ -46,26 +68,7 @@ public class DeadReckoning : Photon.MonoBehaviour, IPunObservable
 	}
 
 
-	public void Update()
-	{
-		if (this.photonView.isMine)
-		{
-			return;     // if this object is under our control, we don't need to apply received position-updates 
-		}
-
-		// We get 10 updates per sec. Sometimes a few less or one or two more, depending on variation of lag.
-		// Due to that we want to reach the correct position in a little over 100ms. We get a new update then.
-		// This way, we can usually avoid a stop of our interpolated cube movement.
-		//
-		// Lerp() gets a fraction value between 0 and 1. This is how far we went from A to B.
-		//
-		// So in 100 ms, we want to move from our previous position to the latest known. 
-		// Our fraction variable should reach 1 in 100ms, so we should multiply deltaTime by 10.
-		// We want it to take a bit longer, so we multiply with 9 instead!
-
-		this.fraction = this.fraction + Time.deltaTime * 9;
-		transform.localPosition = Vector3.Lerp(this.onUpdatePos, this.latestCorrectPos, this.fraction); // set our pos between A and B
-	}
+	
 
 
 
